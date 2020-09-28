b86ddf1417d99c0c41b08c49a731989ba605e54a
a <a@a.com>
2020-09-28 19:49:51 +0200

    primera web

--- psp/testWeb/src/main/java/servlets/Servlet.java
+++ psp/testWeb/src/main/java/servlets/Servlet.java
@@ -1,3 +1,5 @@
+package servlets;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Servlet",urlPatterns = {"/test"})
+@WebServlet(name = "servlets.Servlet",urlPatterns = {"/test"})
 public class Servlet extends HttpServlet {
 
 
