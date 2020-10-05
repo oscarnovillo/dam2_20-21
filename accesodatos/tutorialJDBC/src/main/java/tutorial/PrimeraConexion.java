5b0d77f41b8e621d4a01a532a16f906ad6c148bb
oscar.novillo <oscar.novillo@gmail.com>
2020-10-05 23:06:08 +0200

    aaa

---
+++ accesodatos/tutorialJDBC/src/main/java/tutorial/PrimeraConexion.java
@@ -0,0 +1,52 @@
+package tutorial;
+
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Logger;
+
+public class PrimeraConexion {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        Connection connection = null;
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://dam2.mysql.iesquevedo.es:3335/netflisssss",
+                "root",
+                "root");
+
+        System.out.println("234.34".chars().allMatch(value -> Character.isDigit(value)));
+        Statement stmt = null;
+        ResultSet rs=null;
+        try {
+
+            stmt = connection.createStatement();
+
+            rs = stmt.executeQuery("select * from table_fechas2");
+
+            while (rs.next())
+            {
+                System.out.print(rs.getInt("id"));
+                System.out.print(" "+rs.getString("name"));
+                System.out.print(" "+rs.getTimestamp("date").toLocalDateTime()
+                        .format(DateTimeFormatter.ofPattern("YYYY-MM-dd")));
+                System.out.print(" "+rs.getInt("numero"));
+                System.out.println();
+            }
+
+        } catch (Exception e) {
+            Logger.getLogger("Main").info(e.getMessage());
+        } finally {
+            rs.close();
+            stmt.close();
+            connection.close();
+        }
+    }
+}
