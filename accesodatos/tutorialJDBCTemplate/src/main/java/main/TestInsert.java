0e780a403d01967ce917e1d8c224e1e832eb382c
oscar.novillo <oscar.novillo@gmail.com>
2020-11-17 21:04:45 +0100

    asd

--- accesodatos/tutorialJDBCTemplate/src/main/java/main/TestInsert.java
+++ accesodatos/tutorialJDBCTemplate/src/main/java/main/TestInsert.java
@@ -4,7 +4,6 @@ package main;
 import dao.modelo.Asignatura;
 import dao.utils.CreateModel;
 import dao.utils.DBConnection;
-import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -19,7 +18,6 @@ import java.util.Map;
 
 public class TestInsert {
 
-
     public static void main(String[] args) {
         CreateModel.createModel();
         DBConnection db = new DBConnection();
@@ -27,23 +25,20 @@ public class TestInsert {
                 db.getDataSource())
                 .withTableName("asignaturas")
                 .usingGeneratedKeyColumns("ID");
+        Asignatura a = new Asignatura();
+        a.setCiclo("ciclo");
+        a.setCurso("curso");
+        a.setNombre("nombre");
+        a.setId(200);
         Map<String, Object> parameters = new HashMap<>();
 
-        parameters.put("NOMBRE", "a.getNombre()");
-        parameters.put("CICLO", "a.getCiclo()");
-        parameters.put("CURSO", "a.getCurso()");
-        System.out.println(jdbcInsert.executeAndReturnKey(parameters).longValue());
-
-        Asignatura a = new Asignatura(1,"nombre","cliclo","curso");
-
-
+        parameters.put("NOMBRE", a.getNombre());
+        parameters.put("CICLO", a.getCiclo());
+        parameters.put("CURSO", a.getCurso());
         SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(a);
-        jdbcInsert = new SimpleJdbcInsert(
-                db.getDataSource())
-                .withTableName("asignaturas")
-                .usingGeneratedKeyColumns("ID");
-        System.out.println(jdbcInsert.executeAndReturnKey(namedParameters).longValue());
+        a.setId((int) jdbcInsert.executeAndReturnKey(namedParameters).longValue());
 
+        System.out.println(a);
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         JdbcTemplate jtm = new JdbcTemplate(
@@ -60,7 +55,6 @@ public class TestInsert {
         }, keyHolder);
 
         System.out.println((int) keyHolder.getKey());
-
 
     }
 }
